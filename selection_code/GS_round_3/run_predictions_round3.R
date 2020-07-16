2c479ec98ebb3ff55f1600b03f6167c4cd82212e
Chris Hernandez <36375878+ch728@users.noreply.github.com>
2020-07-16 11:03:46 -0400

    Clean code

--- selection_code/GS_round_3/run_predictions_round3.R
+++ selection_code/GS_round_3/run_predictions_round3.R
@@ -1,35 +1,31 @@
 library(asreml)
 library(sommer)
 library(reshape2)
-source("~/scripts/atcg1234.R")
 
-#read in  GS3.1 pheno
+### Read in phenotpe data ###
 pheno<-read.csv("../../../../GS3.1/phenotypes/ALL/GS3.1_phenotypes.csv")
-str(pheno)
 
-#read in GS.3.1 + GS4 genotype table
+### Read in genotype data ###
 geno<-read.table("../../../RE_TASSEL/Genotype_tables/GS4_GS3.1_RD2_MAF0.05_0.8_0.6.txt",colClasses="character",header=T,row.names=1)
 geno2<-t(apply(geno,1,function(x) gsub("0","N",x)))
 geno2<-t(apply(geno2,1,function(x) gsub("-","N",x)))
-str(geno)
-table(c(as.matrix(geno)))
 
-#get dosage
+### Get dosage ###
 dosage<-atcg1234_2(geno,maf=0.05,imp=F)
 dosage2<-atcg1234_2(geno2,maf=0.05,imp=F)
 #get Amat
 Amat1<-A.mat(dosage,impute.method="EM")
   
-#get inverse
+### Get inverse ###
 A<-Amat1
 diag(A)<-diag(A)+0.00000001
 Ginv<-solve(A)
 
-#subset pheno so that pheno contains only individuals in Amat1
+### Subset pheno so that pheno contains only individuals in Amat1 ###
 phenoSubset<-pheno[pheno$gid %in% rownames(Amat1),]
 phenoSubset$gid<-factor(phenoSubset$gid)
 
-#fit models
+### Fit model ###
 asremlFit<-asreml(scale(cbind(Brix,Dry_matter,a))~trait
                     ,random=~us(trait):ped(gid)
                     ,rcov= ~ units:us(trait)
@@ -44,7 +40,7 @@ asremlFit2<-asreml(scale(cbind(Brix,Dry_matter,a))~trait+Date_Processed:trait
                   ,ginverse=list(gid=as.matrix(Ginv))
                   ,maxiter=250, control=list(workspace=1.6e+9))
 
-#extract predictions
+### Extract predictions ###
 asremlPred<-as.data.frame(asremlFit$coefficients$random)
 colnames(asremlPred)[1]<-"GEBV"
 asremlPred$gid<-gsub("trait_.*ped\\(gid\\)_","",rownames(asremlPred))
@@ -55,26 +51,5 @@ asremlIndx<-as.data.frame(rowSums(asremlPred))
 asremlIndx<-asremlIndx[grep("GS4_",rownames(asremlIndx)),,drop=F]
 asremlIndxSorted<-asremlIndx[order(-asremlIndx$`rowSums(asremlPred)`),,drop=F]
 
-
-asremlPred2<-as.data.frame(asremlFit2$coefficients$random)
-colnames(asremlPred2)[1]<-"GEBV"
-asremlPred2$gid<-gsub("trait_.*ped\\(gid\\)_","",rownames(asremlPred2))
-asremlPred2$trait<-gsub("trait_(.*):ped\\(gid\\)_.*","\\1",rownames(asremlPred2))
-asremlPred2<-melt(asremlPred2)
-asremlPred2<-acast(asremlPred2,gid~trait)
-asremlIndx2<-as.data.frame(rowSums(asremlPred2))
-asremlIndx2<-asremlIndx2[grep("GS4_",rownames(asremlIndx2)),,drop=F]
-asremlIndxSorted2<-asremlIndx2[order(-asremlIndx2$`rowSums(asremlPred2)`),,drop=F]
-
-#compare both model top selections
-sum(rownames(asremlIndxSorted2)[1:20]%in%rownames(asremlIndxSorted)[1:20])
-
-#compare predicted with actual phenotypes for GS3.1
-
-
-#output selections
-write.csv(asremlIndxSorted[1:30,,F],"./GS4_selections_without_day_effect.csv")
-write.csv(asremlIndxSorted2[1:30,,F],"./GS4_selections_with_day_effect.csv")
-
-
-}
+### Output selections ###
+write.csv(asremlIndxSorted[1:30,,F],"selections.csv")
